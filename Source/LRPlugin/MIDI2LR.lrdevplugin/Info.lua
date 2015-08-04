fd2041c7050d7f13889f7ba8eca5f296285c673e
TheGouger <parth@sentryscientific.com>
2015-08-03 21:54:41 -0400

    More tab to space

--- Source/LRPlugin/MIDI2LR.lrdevplugin/Info.lua
+++ Source/LRPlugin/MIDI2LR.lrdevplugin/Info.lua
@@ -7,14 +7,14 @@ MIDI2LR Plugin properties
 ------------------------------------------------------------------------------]]
 
 return {
-	LrSdkVersion = 3.0,
-	LrToolkitIdentifier = 'ca.parth.midi2lr',
-	LrPluginName = 'MIDI2LR',
-	LrInitPlugin = 'MIDI2LR_Client.lua', -- Main client logic
-	LrForceInitPlugin = true,
-	LrExportMenuItems = { -- Needs to provide at least a menu item
-		title = "Start",
-		file = "client.lua",
-	},
-	VERSION = { major=0, minor=1}
+    LrSdkVersion = 3.0,
+    LrToolkitIdentifier = 'ca.parth.midi2lr',
+    LrPluginName = 'MIDI2LR',
+    LrInitPlugin = 'MIDI2LR_Client.lua', -- Main client logic
+    LrForceInitPlugin = true,
+    LrExportMenuItems = { -- Needs to provide at least a menu item
+        title = "Start",
+        file = "client.lua",
+    },
+    VERSION = { major=0, minor=1}
 }
--- Source/LRPlugin/MIDI2LR.lrdevplugin/MIDI2LR_Client.lua
+++ Source/LRPlugin/MIDI2LR.lrdevplugin/MIDI2LR_Client.lua
@@ -12,39 +12,39 @@ local LrTasks             = import 'LrTasks'
 local LrFunctionContext   = import 'LrFunctionContext'
 
 local function midi_lerp_to_develop(param, midi_value)
-	-- map midi range to develop parameter range
-	local min, max = LrDevelopController.getRange(param)
-	local result = midi_value/127 * (max-min) + min
-	return result
+    -- map midi range to develop parameter range
+    local min, max = LrDevelopController.getRange(param)
+    local result = midi_value/127 * (max-min) + min
+    return result
 end
 
 local function develop_lerp_to_midi(param)
-	-- map develop parameter range to midi range
-	local min, max = LrDevelopController.getRange(param)
-	local result = (LrDevelopController.getValue(param)-min)/(max-min) * 127
-	return result
+    -- map develop parameter range to midi range
+    local min, max = LrDevelopController.getRange(param)
+    local result = (LrDevelopController.getValue(param)-min)/(max-min) * 127
+    return result
 end
  
 local function updateParam(param, midi_value)
-	-- this function does a 'pickup' type of check
-	-- that is, it will ensure the develop parameter is close 
-	-- to what the inputted command value is before updating it
-	if LrApplicationView.getCurrentModuleName() ~= 'develop' then
-			LrApplicationView.switchToModule('develop')
-	end
-	
-	--if(math.abs(midi_value - develop_lerp_to_midi(param)) <= 5) then
-		LrDevelopController.setValue(param, midi_lerp_to_develop(param, midi_value))
-	--end
+    -- this function does a 'pickup' type of check
+    -- that is, it will ensure the develop parameter is close 
+    -- to what the inputted command value is before updating it
+    if LrApplicationView.getCurrentModuleName() ~= 'develop' then
+            LrApplicationView.switchToModule('develop')
+    end
+    
+    --if(math.abs(midi_value - develop_lerp_to_midi(param)) <= 5) then
+        LrDevelopController.setValue(param, midi_lerp_to_develop(param, midi_value))
+    --end
 end
 
 -- message processor
 local function processMessage(message)
-	if type(message) == 'string' then
-		-- messages are in the format 'param value'
-		local _, _, param, value = string.find( message, '(%S+)%s(%d+)' )
-		updateParam(param, tonumber(value))
-	end
+    if type(message) == 'string' then
+        -- messages are in the format 'param value'
+        local _, _, param, value = string.find( message, '(%S+)%s(%d+)' )
+        updateParam(param, tonumber(value))
+    end
 end
 
 local PORT = 58763
@@ -52,30 +52,30 @@ local PORT = 58763
 -- Main task
 LrTasks.startAsyncTask( function()
     LrFunctionContext.callWithContext( 'socket_remote', function( context )
-		LrDevelopController.revealAdjustedControls( true ) -- reveal affected parameter in panel track
-		local client = LrSocket.bind {
-			functionContext = context,
-			plugin = _PLUGIN,
-			port = PORT,
-			mode = 'receive',
-			onMessage = function(socket, message)
-				processMessage(message)
-			end,
-			onClosed = function( socket )
-				-- MIDI2LR closed connection, allow for reconnection
-				socket:reconnect()
-			end,
-			onError = function(socket, err)
-				if err == 'timeout' then -- reconnect if timed out
-					socket:reconnect()
-				end
-			end
-		}
-	
-		while true do
-			LrTasks.sleep( 1/2 )
-		end
-		
-		client:close()
-	end )
+        LrDevelopController.revealAdjustedControls( true ) -- reveal affected parameter in panel track
+        local client = LrSocket.bind {
+            functionContext = context,
+            plugin = _PLUGIN,
+            port = PORT,
+            mode = 'receive',
+            onMessage = function(socket, message)
+                processMessage(message)
+            end,
+            onClosed = function( socket )
+                -- MIDI2LR closed connection, allow for reconnection
+                socket:reconnect()
+            end,
+            onError = function(socket, err)
+                if err == 'timeout' then -- reconnect if timed out
+                    socket:reconnect()
+                end
+            end
+        }
+    
+        while true do
+            LrTasks.sleep( 1/2 )
+        end
+        
+        client:close()
+    end )
  end )
