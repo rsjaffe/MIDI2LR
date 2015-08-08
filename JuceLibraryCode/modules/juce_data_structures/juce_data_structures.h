839243c948ddc94ed38999ed0873261c07637549
TheGouger <parth@sentryscientific.com>
2015-08-07 22:40:43 -0400

    Add juce modules to repo for easier compilation

--- JuceLibraryCode/modules/juce_data_structures/juce_data_structures.h
+++ JuceLibraryCode/modules/juce_data_structures/juce_data_structures.h
@@ -1,5 +1,5 @@
 // This is an auto-generated file to redirect any included
 // module headers to the correct external folder.
 
-#include "../../../../../../../juce/modules/juce_data_structures/juce_data_structures.h"
+#include "../../../modules/juce_data_structures/juce_data_structures.h"
 
