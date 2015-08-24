9fc19cede1f501a3cdb9cfb2a8799913f9549537
TheGouger <parth@sentryscientific.com>
2015-08-24 00:05:43 -0400

    proper semantics for launching app

--- Source/LRPlugin/MIDI2LR.lrdevplugin/LaunchAppFromMenu.lua
+++ Source/LRPlugin/MIDI2LR.lrdevplugin/LaunchAppFromMenu.lua
@@ -10,8 +10,8 @@ local LrTasks = import 'LrTasks'
 
 LrTasks.startAsyncTask( function()
     if(WIN_ENV) then
-        LrShell.openFilesInApp({""}, _PLUGIN.path..'/MIDI2LR.exe')
+        LrShell.openFilesInApp({_PLUGIN.path..'/Info.lua'}, _PLUGIN.path..'/MIDI2LR.exe')
     else
-        LrShell.openFilesInApp({""}, _PLUGIN.path..'/MIDI2LR.app')
+        LrShell.openFilesInApp({_PLUGIN.path..'/Info.lua'}, _PLUGIN.path..'/MIDI2LR.app') -- On Mac it seems like the files argument has to include an existing file
     end
 end)
