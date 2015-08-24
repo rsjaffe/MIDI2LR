730904175c1aedff5afa024fbe10de6c22f8d425
Parth <parth@Parths-Mac-Pro.local>
2015-08-23 23:35:47 -0400

    fix argument for autoloading on Mac

--- Source/LRPlugin/MIDI2LR.lrdevplugin/MIDI2LR_Client.lua
+++ Source/LRPlugin/MIDI2LR.lrdevplugin/MIDI2LR_Client.lua
@@ -170,8 +170,8 @@ LrTasks.startAsyncTask( function()
  
 LrTasks.startAsyncTask( function()
     if(WIN_ENV) then
-        LrShell.openFilesInApp({""}, _PLUGIN.path..'/MIDI2LR.exe')
+        LrShell.openFilesInApp({_PLUGIN.path..'/Info.lua'}, _PLUGIN.path..'/MIDI2LR.exe')
     else
-        LrShell.openFilesInApp({""}, _PLUGIN.path..'/MIDI2LR.app')
+        LrShell.openFilesInApp({_PLUGIN.path..'/Info.lua'}, _PLUGIN.path..'/MIDI2LR.app') -- On Mac it seems like the files argument has to include an existing file
     end
 end)
