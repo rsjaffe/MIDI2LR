dec8a84e12aa0cc32a16bb2c52cbc772be9bdbd1
rsjaffe <rsjaffe@gmail.com>
2015-09-29 07:34:56 -0700

    Added support for NRPN messages
    This is an intermediate step. Still have to add constructor and change
    parsing of xml file.

--- Source/CommandMap.cpp
+++ Source/CommandMap.cpp
@@ -1,9 +1,9 @@
 /*
   ==============================================================================
 
-    CommandMap.cpp
-    Created: 31 Jul 2015 9:16:41pm
-    Author:  Parth
+	CommandMap.cpp
+	Created: 31 Jul 2015 9:16:41pm
+	Author:  Parth
 
   ==============================================================================
 */
@@ -20,79 +20,83 @@ CommandMap::CommandMap()
 
 CommandMap& CommandMap::getInstance()
 {
-    static CommandMap instance;
-    return instance;
+	static CommandMap instance;
+	return instance;
 }
 
 void CommandMap::addCommandforMessage(int command, const MIDI_Message &msg)
 {
-    // adds a msg to the msg:command map, and it's associated command to the command:msg map
-    if (command < LRCommandList::LRStringList.size())
-    {
-        _messageMap[msg] = LRCommandList::LRStringList[command];
-        _commandStringMap[LRCommandList::LRStringList[command]] = msg;
-    }
-    else
-        _messageMap[msg] = LRCommandList::ProfileList[command - LRCommandList::LRStringList.size()];
+	// adds a msg to the msg:command map, and it's associated command to the command:msg map
+	if (command < LRCommandList::LRStringList.size())
+	{
+		_messageMap[msg] = LRCommandList::LRStringList[command];
+		_commandStringMap[LRCommandList::LRStringList[command]] = msg;
+	}
+	else
+		_messageMap[msg] = LRCommandList::ProfileList[command - LRCommandList::LRStringList.size()];
 }
 
 void CommandMap::addCommandforMessage(const String& command, const MIDI_Message &msg)
 {
-    _messageMap[msg] = command;
-    _commandStringMap[command] = msg;
+	_messageMap[msg] = command;
+	_commandStringMap[command] = msg;
 }
 
 const String& CommandMap::getCommandforMessage(const MIDI_Message &msg) const
 {
-    return _messageMap.at(msg);
+	return _messageMap.at(msg);
 }
 
 const MIDI_Message& CommandMap::getMessageForCommand(const String &command) const
 {
-    return _commandStringMap.at(command);
+	return _commandStringMap.at(command);
 }
 
 bool CommandMap::messageExistsInMap(const MIDI_Message &msg) const
 {
-    return _messageMap.count(msg);
+	return _messageMap.count(msg);
 }
 
 bool CommandMap::commandHasAssociatedMessage(const String &command) const
 {
-    return _commandStringMap.count(command);
+	return _commandStringMap.count(command);
 }
 
 void CommandMap::removeMessage(const MIDI_Message &msg)
 {
-    // removes msg from the msg:command map, and it's associated command from the command:msg map
-    _commandStringMap.erase(_messageMap[msg]);
-    _messageMap.erase(msg);
+	// removes msg from the msg:command map, and it's associated command from the command:msg map
+	_commandStringMap.erase(_messageMap[msg]);
+	_messageMap.erase(msg);
 }
 
 void CommandMap::clearMap()
 {
-    _commandStringMap.clear();
-    _messageMap.clear();
+	_commandStringMap.clear();
+	_messageMap.clear();
 }
 
 void CommandMap::toXMLDocument(File& file) const
 {
-    // save the contents of the command map to an xml file
-    XmlElement root("settings");
-    for (auto mapEntry : _messageMap)
-    {
-        XmlElement* setting = new XmlElement("setting");
-        setting->setAttribute("channel", mapEntry.first.channel);
+	// save the contents of the command map to an xml file
+	XmlElement root("settings");
+	for (auto mapEntry : _messageMap)
+	{
+		XmlElement* setting = new XmlElement("setting");
+		setting->setAttribute("channel", mapEntry.first.channel);
 
-        if (mapEntry.first.isCC)
-            setting->setAttribute("controller", mapEntry.first.controller);
-        else
-            setting->setAttribute("note", mapEntry.first.pitch);
 
-        setting->setAttribute("command_string", mapEntry.second);
+		setting->setAttribute("NRPN", (mapEntry.first.isNRPN) ? "True" : "False");
+		setting->setAttribute("Relative", (mapEntry.first.isRelative) ? "True" : "False");
 
-        root.addChildElement(setting);
-    }
+		if (mapEntry.first.isCC)
+			setting->setAttribute("controller", mapEntry.first.controller);
+		else
+			setting->setAttribute("note", mapEntry.first.pitch);
 
-    root.writeToFile(file, "");
+		setting->setAttribute("command_string", mapEntry.second);
+
+		root.addChildElement(setting);
+	}
+
+	root.writeToFile(file, "");
 }
--- Source/CommandMap.h
+++ Source/CommandMap.h
@@ -1,9 +1,9 @@
 /*
   ==============================================================================
 
-    CommandMap.h
-    Created: 31 Jul 2015 9:16:41pm
-    Author:  Parth
+	CommandMap.h
+	Created: 31 Jul 2015 9:16:41pm
+	Author:  Parth
 
   ==============================================================================
 */
@@ -17,95 +17,102 @@
 // encapsulates a MIDI message (Note or CC)
 struct MIDI_Message
 {
-    bool isCC;
-    int channel;
-    union {
-        int controller;
-        int pitch;
-        int data;
-    };
+	bool isCC;
+	bool isNRPN;
+	bool isRelative;
+	int channel;
+	union {
+		int controller;
+		int pitch;
+		int data;
+	};
 
-    MIDI_Message() : isCC(0),
-                     channel(0),
-                     data(0)
-    {
-    }
+	MIDI_Message() : isCC(0),
+		isNRPN(0),
+		isRelative(0),
+		channel(0),
+		data(0)
 
-    MIDI_Message(int ch, int dat, bool iscc) : channel(ch),
-                                               data(dat),
-                                               isCC(iscc)
-    {}
+	{
+	}
 
-    bool operator==(const MIDI_Message &other) const
-    {
-        return (isCC == other.isCC && channel == other.channel && data == other.data);
-    }
+	MIDI_Message(int ch, int dat, bool iscc) : channel(ch),
+		data(dat),
+		isCC(iscc),
+		isNRPN(0),
+		isRelative(0)
+	{}
+
+	bool operator==(const MIDI_Message &other) const
+	{
+		return (isCC == other.isCC && channel == other.channel && data == other.data);
+	}
 };
 
 // hash functions for MIDI_Message and String
 namespace std {
-    template <>
-    struct hash<MIDI_Message>
-    {
-        std::size_t operator()(const MIDI_Message& k) const
-        {
-            return (std::hash<bool>()(k.isCC) ^ std::hash<int>()(k.channel) ^ (std::hash<int>()(k.data) << 1));
-        }
-    };
+	template <>
+	struct hash<MIDI_Message>
+	{
+		std::size_t operator()(const MIDI_Message& k) const
+		{
+			return (std::hash<bool>()(k.isCC) ^ std::hash<int>()(k.channel) ^ (std::hash<int>()(k.data) << 1));
+		}
+	};
 
-    template <>
-    struct hash<String>
-    {
-        std::size_t operator()(const String& k) const
-        {
-            return k.hash();
-        }
-    };
+	template <>
+	struct hash<String>
+	{
+		std::size_t operator()(const String& k) const
+		{
+			return k.hash();
+		}
+	};
 }
 
 class CommandMap
 {
 public:
-    static const int UNMAPPED_INDEX;
+	static const int UNMAPPED_INDEX;
 
-    static CommandMap& getInstance();
+	static CommandMap& getInstance();
 
-    // adds an entry to the msg:command map, and a corresponding entry to the command:msg map
-    // will look up the string by the index (but it is preferred to directly use the String)
-    void addCommandforMessage(int command, const MIDI_Message &cc);
-    
-    // adds an entry to the msg:command map, and a corresponding entry to the command:msg map
-    void addCommandforMessage(const String &command, const MIDI_Message &cc);
+	// adds an entry to the msg:command map, and a corresponding entry to the command:msg map
+	// will look up the string by the index (but it is preferred to directly use the String)
+	void addCommandforMessage(int command, const MIDI_Message &cc);
 
-    // gets the LR command associated to a MIDI message
-    const String& getCommandforMessage(const MIDI_Message &msg) const;
+	// adds an entry to the msg:command map, and a corresponding entry to the command:msg map
+	void addCommandforMessage(const String &command, const MIDI_Message &cc);
 
-    // removes a MIDI message from the msg:command map, and it's associated entry in the command:msg map
-    void removeMessage(const MIDI_Message &msg);
+	// gets the LR command associated to a MIDI message
+	const String& getCommandforMessage(const MIDI_Message &msg) const;
 
-    // clears both msg:command and command:msg maps
-    void clearMap();
+	// removes a MIDI message from the msg:command map, and it's associated entry in the command:msg map
+	void removeMessage(const MIDI_Message &msg);
 
-    // returns true if there is a mapping for a particular MIDI message
-    bool messageExistsInMap(const MIDI_Message &msg) const;
+	// clears both msg:command and command:msg maps
+	void clearMap();
 
-    // gets the MIDI message associated to a LR command
-    const MIDI_Message& getMessageForCommand(const String &command) const;
+	// returns true if there is a mapping for a particular MIDI message
+	bool messageExistsInMap(const MIDI_Message &msg) const;
 
-    // returns true if there is a mapping for a particular LR command
-    bool commandHasAssociatedMessage(const String &command) const;
+	// gets the MIDI message associated to a LR command
+	const MIDI_Message& getMessageForCommand(const String &command) const;
 
-    // saves the msg:command map as an XML file
-    void toXMLDocument(File& file) const;
+	// returns true if there is a mapping for a particular LR command
+	bool commandHasAssociatedMessage(const String &command) const;
+
+	// saves the msg:command map as an XML file
+	void toXMLDocument(File& file) const;
 
 private:
-    CommandMap();
+	CommandMap();
 
-    CommandMap(CommandMap const&)  = delete;
-    void operator=(CommandMap const&) = delete;
+	CommandMap(CommandMap const&) = delete;
+	void operator=(CommandMap const&) = delete;
 
-    std::unordered_map<MIDI_Message, String> _messageMap;
-    std::unordered_map<String, MIDI_Message> _commandStringMap;
+	std::unordered_map<MIDI_Message, String> _messageMap;
+	std::unordered_map<String, MIDI_Message> _commandStringMap;
 };
 
 
