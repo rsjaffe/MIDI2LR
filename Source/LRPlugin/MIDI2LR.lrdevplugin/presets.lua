73dd3b5a17d12d2ce66b8de8a1502f7de32caec7
rsjaffe <rsjaffe@gmail.com>
2015-11-17 13:48:40 -0800

    Binding observable table to dialog

--- Source/LRPlugin/MIDI2LR.lrdevplugin/presets.lua
+++ Source/LRPlugin/MIDI2LR.lrdevplugin/presets.lua
@@ -1,22 +1,39 @@
+--[[----------------------------------------------------------------------------
+presets.lua
+Enumerates presets in Lightroom for MIDI2LR
+Saves selected presets for mapping to MIDI2LR commands
+------------------------------------------------------------------------------]]
+
 local LrApplication = import 'LrApplication'
+local LrBinding = import 'LrBinding'
 local LrDialogs = import 'LrDialogs'
+local LrFunctionContext = import 'LrFunctionContext'
 local LrView = import 'LrView'
 local psList = {}
 
-for _,fold in pairs(LrApplication.developPresetFolders()) do
-  local foldname = fold:getName()
-  for _,pst in pairs(fold:getDevelopPresets()) do
-    table.insert(psList,{title = foldname..'->'..pst:getName(), value = pst:getUuid()})
-  end
+local function assignPresets()
+  LrFunctionContext.callWithContext( "assignPresets", function( context )
+
+      for _,fold in pairs(LrApplication.developPresetFolders()) do
+        local foldname = fold:getName()
+        for _,pst in pairs(fold:getDevelopPresets()) do
+          table.insert(psList,{title = foldname..'->'..pst:getName(), value = pst:getUuid()})
+        end
+      end
+
+      local props = LrBinding.makePropertyTable( context )
+--      props.myObservedString = "This is a string"
+
+      local f = LrView.osFactory()
+      local font = "<system/small>"
+      LrDialogs.presentModalDialog {title = "Assign Presets", resizable = true,
+        contents = f:column {
+          bind_to_object = props,
+          f:static_text {title = "Available Presets", font = font},
+          f:simple_list {font = font, items = psList},
+        }
+      }
+    end )
 end
 
-local f = LrView.osFactory()
-local font = "<system/small>"
-LrDialogs.presentModalDialog {title = "Test", resizable = true,
-  contents = f:column {
-    f:static_text {title = "Hello world", font = font},
-    f:simple_list {font = font,
-      items = psList
-    }
-  }
-}
+assignPresets() --execute
