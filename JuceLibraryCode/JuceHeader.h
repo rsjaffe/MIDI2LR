ffbe78721356fe026cd6a492fc24667798879db1
rsjaffe <rsjaffe@gmail.com>
2015-11-27 13:39:09 -0800

    update to v0.6.0, fix OSX build settings

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -39,8 +39,8 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "MIDI2LR";
-    const char* const  versionString  = "0.5.0";
-    const int          versionNumber  = 0x500;
+    const char* const  versionString  = "0.6.0";
+    const int          versionNumber  = 0x600;
 }
 #endif
 
--- Source/LRPlugin/MIDI2LR.lrdevplugin/Info.lua
+++ Source/LRPlugin/MIDI2LR.lrdevplugin/Info.lua
@@ -34,5 +34,5 @@ return {
             file = "LaunchAppFromMenu.lua"
         },
     },
-    VERSION = { major=0, minor=5, revision=0}
+    VERSION = { major=0, minor=6, revision=0}
 }
