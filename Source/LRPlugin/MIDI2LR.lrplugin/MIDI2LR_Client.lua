1eb880dd347876cb5e6502406f3047851247cca9
rsjaffe <rsjaffe@gmail.com>
2015-12-27 19:24:44 -0800

    Squashed paste selective bug
    needed to add 2012 values

--- Source/LRPlugin/MIDI2LR.lrplugin/MIDI2LR_Client.lua
+++ Source/LRPlugin/MIDI2LR.lrplugin/MIDI2LR_Client.lua
@@ -36,53 +36,9 @@ local LrUndo              = import 'LrUndo'
 currentLoadVersion = rawget (_G, 'currentLoadVersion') or 0  
 currentLoadVersion = currentLoadVersion + 1 
 
---[[-----------debug section, enable by adding - to beginning this line
-local LrLogger = import 'LrLogger'
-
-local myLogger = LrLogger( 'libraryLogger' )
-myLogger:enable( 'logfile' ) -- Pass either a string or a table of actions.
-
--- Write trace information to the logger.
-
-local function outputToLog( message )
-  myLogger:trace( message )
-end
----table.tostring function
-function table.val_to_str ( v )
-  if 'string' == type( v ) then
-    v = string.gsub( v, '\n', '\\n' )
-    if string.match( string.gsub(v,'[^'\']',''), '^'+$' ) then
-      return ''' .. v .. '''
-    end
-    return ''' .. string.gsub(v,''', '\\'' ) .. '''
-  else
-    return 'table' == type( v ) and table.tostring( v ) or
-    tostring( v )
-  end
-end
-
-function table.key_to_str ( k )
-  if 'string' == type( k ) and string.match( k, '^[_%a][_%a%d]*$' ) then
-    return k
-  else
-    return '[' .. table.val_to_str( k ) .. ']'
-  end
-end
-
-function table.tostring( tbl )
-  local result, done = {}, {}
-  for k, v in ipairs( tbl ) do
-    table.insert( result, table.val_to_str( v ) )
-    done[ k ] = true
-  end
-  for k, v in pairs( tbl ) do
-    if not done[ k ] then
-      table.insert( result,
-        table.key_to_str( k ) .. '=' .. table.val_to_str( v ) )
-    end
-  end
-  return '{' .. table.concat( result, ',' ) .. '}'
-end
+---[[-----------debug section, enable by adding - to beginning this line
+local LrMobdebug = import 'LrMobdebug'
+LrMobdebug.start()
 --]]-----------end debug section
 
 
