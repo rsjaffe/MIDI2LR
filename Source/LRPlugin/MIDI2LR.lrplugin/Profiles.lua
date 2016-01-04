1d35a79c53c1926c2bb1bc502f4d1c5160175ca1
rsjaffe <rsjaffe@gmail.com>
2016-01-03 21:06:28 -0800

    fixes in profiles.lua

--- Source/LRPlugin/MIDI2LR.lrplugin/Profiles.lua
+++ Source/LRPlugin/MIDI2LR.lrplugin/Profiles.lua
@@ -18,42 +18,42 @@ You should have received a copy of the GNU General Public License along with
 MIDI2LR.  If not, see <http://www.gnu.org/licenses/>. 
 ------------------------------------------------------------------------------]]
 
-local LrApplicationView  = import 'LrApplicationView'
+local LrApplicationView   = import 'LrApplicationView'
 local LrDevelopController = import 'LrDevelopController'
 
-local defaults = {
-  loupe = {ToolModulePanel = 'Tool', presetFile = '', friendlyName = LOC("$$$/AgPhotoBin/ViewMode/Develop/Loupe=Loupe"),},
-  crop = {ToolModulePanel = 'Tool', presetFile = '', friendlyName = LOC("$$$/AgCameraRawNamedSettings/SaveNamedDialog/Crop=Crop"),},
-  dust = {ToolModulePanel = 'Tool', presetFile = '', friendlyName = LOC("$$$/AgCameraRawNamedSettings/SaveNamedDialog/SpotRemoval=Spot Removal"),},
-  redeye = {ToolModulePanel = 'Tool', presetFile = '', friendlyName = LOC("$$$/AgCameraRawNamedSettings/CameraRawSettingMapping/Redeye=Red-eye Correction"),},
-  gradient = {ToolModulePanel = 'Tool', presetFile = '', friendlyName = LOC("$$$/AgCameraRawNamedSettings/SaveNamedDialog/GraduatedFilters=Graduated Filters"),},
-  circularGradient = {ToolModulePanel = 'Tool', presetFile = '', friendlyName = LOC("$$$/AgCameraRawNamedSettings/SaveNamedDialog/RadialFilters=Radial Filters"),},
-  localized = {ToolModulePanel = 'Tool', presetFile = '', friendlyName = LOC("$$$/AgCameraRawNamedSettings/SaveNamedDialog/BrushAdjustments=Brush Adjustments"),},
-  adjustPanel = {ToolModulePanel = 'Panel', presetFile = '', friendlyName = LOC("$$$/AgDevelop/Panel/BasicAdjustments=Basic Adjustments"),},
-  tonePanel = {ToolModulePanel = 'Panel', presetFile = '', friendlyName = LOC("$$$/AgDevelop/Panel/ToneCurve=Tone Curve"),},
-  mixerPanel = {ToolModulePanel = 'Panel', presetFile = '', friendlyName = LOC("$$$/AgCameraRawNamedSettings/SaveNamedDialog/ColorAdjustments=Color Adjustments"),},
-  splitToningPanel = {ToolModulePanel = 'Panel', presetFile = '', friendlyName = LOC("$$$/AgDevelop/Panel/SplitToning=Split Toning"),},
-  detailPanel = {ToolModulePanel = 'Panel', presetFile = '', friendlyName = LOC("$$$/AgDevelop/Panel/Detail=Details")},
-  lensCorrectionsPanel = {ToolModulePanel = 'Panel', presetFile = '', friendlyName = LOC("$$$/AgDevelop/Panel/LensCorrections=Lens Corrections"),},
-  effectsPanel = {ToolModulePanel = 'Panel', presetFile = '', friendlyName = LOC("$$$/AgDevelop/Panel/Effects=Effects"),},
-  calibratePanel = {ToolModulePanel = 'Panel', presetFile = '', friendlyName = LOC("$$$/AgDevelop/Panel/Calibration=Camera Calibration"),},
-  library = {ToolModulePanel = 'Module', presetFile = '', friendlyName = LOC("$$$/AgLibraryModule/ModuleTitle=Library"),},
-  develop = {ToolModulePanel = 'Module', presetFile = '', friendlyName = LOC("$$$/SmartCollection/Criteria/Heading/Develop=Develop"),},
-  map = {ToolModulePanel = 'Module', presetFile = '', friendlyName = LOC("$$$/AgLocation/ModuleTitle=Map"),},
-  book = {ToolModulePanel = 'Module', presetFile = '', friendlyName = LOC("$$$/Ag/Layout/Book/ModuleTitle=Book"),},
-  slideshow = {ToolModulePanel = 'Module', presetFile = '', friendlyName = LOC("$$$/AgApplication/Menu/Window/SecondMonitor/Slideshow=Slideshow"),},
-  print = {ToolModulePanel = 'Module', presetFile = '', friendlyName = LOC("$$$/AgPrint/Menu/File/Print=Print"),},
-  web = {ToolModulePanel = 'Module', presetFile = '', friendlyName = LOC("$$$/WPG/Help/Shortcuts/WebHeader=Web"),},
-  profile1 = {ToolModulePanel = '', presetFile = '', friendlyName = LOC("$$$/AgDevelop/CameraRawPanel/Profile=Profile:")..' 1',},
-  profile2 = {ToolModulePanel = '', presetFile = '', friendlyName = LOC("$$$/AgDevelop/CameraRawPanel/Profile=Profile:")..' 2',},
-  profile3 = {ToolModulePanel = '', presetFile = '', friendlyName = LOC("$$$/AgDevelop/CameraRawPanel/Profile=Profile:")..' 3',},
-  profile4 = {ToolModulePanel = '', presetFile = '', friendlyName = LOC("$$$/AgDevelop/CameraRawPanel/Profile=Profile:")..' 4',},
-  profile5 = {ToolModulePanel = '', presetFile = '', friendlyName = LOC("$$$/AgDevelop/CameraRawPanel/Profile=Profile:")..' 5',},
-  profile6 = {ToolModulePanel = '', presetFile = '', friendlyName = LOC("$$$/AgDevelop/CameraRawPanel/Profile=Profile:")..' 6',},
-  profile7 = {ToolModulePanel = '', presetFile = '', friendlyName = LOC("$$$/AgDevelop/CameraRawPanel/Profile=Profile:")..' 7',},
-  profile8 = {ToolModulePanel = '', presetFile = '', friendlyName = LOC("$$$/AgDevelop/CameraRawPanel/Profile=Profile:")..' 8',},
-  profile9 = {ToolModulePanel = '', presetFile = '', friendlyName = LOC("$$$/AgDevelop/CameraRawPanel/Profile=Profile:")..' 9',},
-  profile10 = {ToolModulePanel = '', presetFile = '', friendlyName = LOC("$$$/AgDevelop/CameraRawPanel/Profile=Profile:")..' 10',},
+local ProfileTypes = {
+  loupe = {ToolModulePanel = 'Tool', friendlyName = LOC("$$$/AgPhotoBin/ViewMode/Develop/Loupe=Loupe"),},
+  crop = {ToolModulePanel = 'Tool', friendlyName = LOC("$$$/AgCameraRawNamedSettings/SaveNamedDialog/Crop=Crop"),},
+  dust = {ToolModulePanel = 'Tool', friendlyName = LOC("$$$/AgCameraRawNamedSettings/SaveNamedDialog/SpotRemoval=Spot Removal"),},
+  redeye = {ToolModulePanel = 'Tool', friendlyName = LOC("$$$/AgCameraRawNamedSettings/CameraRawSettingMapping/Redeye=Red-eye Correction"),},
+  gradient = {ToolModulePanel = 'Tool', friendlyName = LOC("$$$/AgCameraRawNamedSettings/SaveNamedDialog/GraduatedFilters=Graduated Filters"),},
+  circularGradient = {ToolModulePanel = 'Tool', friendlyName = LOC("$$$/AgCameraRawNamedSettings/SaveNamedDialog/RadialFilters=Radial Filters"),},
+  localized = {ToolModulePanel = 'Tool', friendlyName = LOC("$$$/AgCameraRawNamedSettings/SaveNamedDialog/BrushAdjustments=Brush Adjustments"),},
+  adjustPanel = {ToolModulePanel = 'Panel', friendlyName = LOC("$$$/AgDevelop/Panel/BasicAdjustments=Basic Adjustments"),},
+  tonePanel = {ToolModulePanel = 'Panel', friendlyName = LOC("$$$/AgDevelop/Panel/ToneCurve=Tone Curve"),},
+  mixerPanel = {ToolModulePanel = 'Panel', friendlyName = LOC("$$$/AgCameraRawNamedSettings/SaveNamedDialog/ColorAdjustments=Color Adjustments"),},
+  splitToningPanel = {ToolModulePanel = 'Panel', friendlyName = LOC("$$$/AgDevelop/Panel/SplitToning=Split Toning"),},
+  detailPanel = {ToolModulePanel = 'Panel', friendlyName = LOC("$$$/AgDevelop/Panel/Detail=Details")},
+  lensCorrectionsPanel = {ToolModulePanel = 'Panel', friendlyName = LOC("$$$/AgDevelop/Panel/LensCorrections=Lens Corrections"),},
+  effectsPanel = {ToolModulePanel = 'Panel', friendlyName = LOC("$$$/AgDevelop/Panel/Effects=Effects"),},
+  calibratePanel = {ToolModulePanel = 'Panel', friendlyName = LOC("$$$/AgDevelop/Panel/Calibration=Camera Calibration"),},
+  library = {ToolModulePanel = 'Module', friendlyName = LOC("$$$/AgLibraryModule/ModuleTitle=Library"),},
+  develop = {ToolModulePanel = 'Module', friendlyName = LOC("$$$/SmartCollection/Criteria/Heading/Develop=Develop"),},
+  map = {ToolModulePanel = 'Module', friendlyName = LOC("$$$/AgLocation/ModuleTitle=Map"),},
+  book = {ToolModulePanel = 'Module', friendlyName = LOC("$$$/Ag/Layout/Book/ModuleTitle=Book"),},
+  slideshow = {ToolModulePanel = 'Module', friendlyName = LOC("$$$/AgApplication/Menu/Window/SecondMonitor/Slideshow=Slideshow"),},
+  print = {ToolModulePanel = 'Module', friendlyName = LOC("$$$/AgPrint/Menu/File/Print=Print"),},
+  web = {ToolModulePanel = 'Module', friendlyName = LOC("$$$/WPG/Help/Shortcuts/WebHeader=Web"),},
+  profile1 = {ToolModulePanel = '', friendlyName = LOC("$$$/AgDevelop/CameraRawPanel/Profile=Profile:")..' 1',},
+  profile2 = {ToolModulePanel = '', friendlyName = LOC("$$$/AgDevelop/CameraRawPanel/Profile=Profile:")..' 2',},
+  profile3 = {ToolModulePanel = '', friendlyName = LOC("$$$/AgDevelop/CameraRawPanel/Profile=Profile:")..' 3',},
+  profile4 = {ToolModulePanel = '', friendlyName = LOC("$$$/AgDevelop/CameraRawPanel/Profile=Profile:")..' 4',},
+  profile5 = {ToolModulePanel = '', friendlyName = LOC("$$$/AgDevelop/CameraRawPanel/Profile=Profile:")..' 5',},
+  profile6 = {ToolModulePanel = '', friendlyName = LOC("$$$/AgDevelop/CameraRawPanel/Profile=Profile:")..' 6',},
+  profile7 = {ToolModulePanel = '', friendlyName = LOC("$$$/AgDevelop/CameraRawPanel/Profile=Profile:")..' 7',},
+  profile8 = {ToolModulePanel = '', friendlyName = LOC("$$$/AgDevelop/CameraRawPanel/Profile=Profile:")..' 8',},
+  profile9 = {ToolModulePanel = '', friendlyName = LOC("$$$/AgDevelop/CameraRawPanel/Profile=Profile:")..' 9',},
+  profile10 = {ToolModulePanel = '', friendlyName = LOC("$$$/AgDevelop/CameraRawPanel/Profile=Profile:")..' 10',},
 }
 
 local current = {Tool = '', Module = '', Panel = ''}
@@ -61,17 +61,17 @@ local current = {Tool = '', Module = '', Panel = ''}
 
 local function useDefaults()
   ProgramPreferences.Profiles = {}
-  for k,v in pairs(defaults) do
-    ProgramPreferences.Profiles[k] = v
+  for k in pairs(ProfileTypes) do
+    ProgramPreferences.Profiles[k] = ''
   end
 end
 
 local function changeProfile(profilename, ignoreCurrent)
   local changed = false
-  if defaults[profilename] ~= nil then
-    local newprofile_file = ProgramPreferences.Profiles[profilename]['presetFile']
-    local newToolModulePanel = ProgramPreferences.Profiles[profilename]['ToolModulePanel']
-    if (newprofile_file) and (newprofile_file ~= '') and 
+  if profilename and ProfileTypes[profilename] then
+    local newprofile_file = ProgramPreferences.Profiles[profilename]
+    local newToolModulePanel = ProfileTypes[profilename]['ToolModulePanel']
+    if (newprofile_file ~= '') and 
     ((ignoreCurrent == true) or (newToolModulePanel == '') or (current[newToolModulePanel] ~= profilename)) then
       MIDI2LR.SERVER:send('SwitchProfile '..newprofile_file..'\n')
       if newToolModulePanel ~= '' then
@@ -85,18 +85,27 @@ end
 
 local function checkProfile()
   local newmod = LrApplicationView.getCurrentModuleName()
-  if changeProfile(newmod) ~= true and newmod == 'develop' then
+  changeProfile(newmod)
+  if newmod == 'develop' then --this may cause problems if getSelectedTool always returns something--need to verify ok
     changeProfile(LrDevelopController.getSelectedTool())
   end
 end
 
 local function StartDialog(obstable,f)
+  for k in pairs(ProfileTypes) do
+    obstable['Profile'..k] = ProgramPreferences.Profiles[k]
+  end
+  --design dialog portion here and return it
 end
 
 local function EndDialog(obstable, status)
   if status == 'ok' then
     useDefaults() -- empty out prior settings
-    -- put other stuff here
+    for k in pairs(ProfileTypes) do
+      if type(obstable['Profile'..k])=='string' then
+        ProgramPreferences.Profiles[k] = obstable['Profile'..k]
+      end
+    end
   end
 end
 
