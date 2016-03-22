209ee2760e3693804468d5c1ba1b3d68da4f91a4
rsjaffe <rsjaffe@gmail.com>
2016-03-21 19:31:13 -0700

    more work on keys dialog

--- Source/LRPlugin/MIDI2LR.lrplugin/Keys.lua
+++ Source/LRPlugin/MIDI2LR.lrplugin/Keys.lua
@@ -1,4 +1,5 @@
 local LrStringUtils = import 'LrStringUtils'
+local LrView = import 'LrView'
 
 local legalanswers = {
   ['spacebar'] = true,
@@ -37,12 +38,38 @@ local legalanswers = {
   f15 = true,
   f16 = true,
 }
+local maxlength = 0
+local completion = {}
+for k in pairs(legalanswers) do
+  maxlength = math.max(k:len(),maxlength)
+  table.insert(completion,k)
+end
 
-function validate(view,value)
+
+local function validate(_,value)
   value = LrStringUtils.trimWhitespace(value)
-  local _, count = string.gsub(value, "[^\128-\193]", "") -- UTF-8 characters
+  local _, count = value:gsub( "[^\128-\193]", "") -- UTF-8 characters
   if count == 1 then return true,value end
   value = LrStringUtils.lower(value)
   if legalanswers[value] then return true,value end
   return false, '', 'Value must be single character or spell out an F key (F1-F16) or: spacebar, return, escape, backspace, cursor left, cursor right, cursor up, cursor down, page up, page down, home, end, delete, insert, tab, play, stop, fast forward, rewind.'
 end
+--startdialog nowhere near ready--do not use yet!
+local function StartDialog(obstable,f)
+  local row = { 
+    f:checkbox{title = 'control', value = LrView.bind('control')},
+    f:checkbox{title = 'alt', value = LrView.bind('alt')},
+    f:checkbox{title = 'shift', value = LrView.bind('shift')},
+    f:edit_field{value = LrView.bind('key'), validate = validate, immediate = false, auto_completion = true, completion = completion, width_in_chars = maxlength} }
+  return row
+end
+
+local function EndDialog(obstable, status)
+  if status == 'ok' then
+  end
+end
+
+return { --table of exports, setting table member name and module function it points to
+  EndDialog   = EndDialog,
+  StartDialog = StartDialog,
+}
