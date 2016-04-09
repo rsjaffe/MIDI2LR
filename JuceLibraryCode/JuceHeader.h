46216704fa7b12e6f10f8c022443e8e8633bb28c
rsjaffe <rsjaffe@gmail.com>
2016-04-09 09:30:47 -0700

    Update Juce to 4.2

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -14,15 +14,17 @@
 #define __APPHEADERFILE_JQAEEL__
 
 #include "AppConfig.h"
-#include "modules/juce_audio_basics/juce_audio_basics.h"
-#include "modules/juce_audio_devices/juce_audio_devices.h"
-#include "modules/juce_audio_formats/juce_audio_formats.h"
-#include "modules/juce_core/juce_core.h"
-#include "modules/juce_data_structures/juce_data_structures.h"
-#include "modules/juce_events/juce_events.h"
-#include "modules/juce_graphics/juce_graphics.h"
-#include "modules/juce_gui_basics/juce_gui_basics.h"
-#include "modules/juce_gui_extra/juce_gui_extra.h"
+
+#include <juce_audio_basics/juce_audio_basics.h>
+#include <juce_audio_devices/juce_audio_devices.h>
+#include <juce_audio_formats/juce_audio_formats.h>
+#include <juce_core/juce_core.h>
+#include <juce_data_structures/juce_data_structures.h>
+#include <juce_events/juce_events.h>
+#include <juce_graphics/juce_graphics.h>
+#include <juce_gui_basics/juce_gui_basics.h>
+#include <juce_gui_extra/juce_gui_extra.h>
+
 #include "BinaryData.h"
 
 #if ! DONT_SET_USING_JUCE_NAMESPACE
