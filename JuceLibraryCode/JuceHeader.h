9d6a3f4b939f673a4d9218e1e261df2d32acc69f
rsjaffe <rsjaffe@gmail.com>
2016-04-10 18:02:24 -0700

    Update version

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -37,8 +37,8 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "MIDI2LR";
-    const char* const  versionString  = "0.9.10.1";
-    const int          versionNumber  = 0x90a01;
+    const char* const  versionString  = "1.0.0.0";
+    const int          versionNumber  = 0x1000000;
 }
 #endif
 
