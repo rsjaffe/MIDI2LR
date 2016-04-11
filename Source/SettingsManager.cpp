61419871b3ec64063804c4621e44712bdd00d0a4
rsjaffe <rsjaffe@gmail.com>
2016-04-10 17:19:33 -0700

    Update make_unique definition

--- Source/SettingsManager.cpp
+++ Source/SettingsManager.cpp
@@ -27,7 +27,7 @@ namespace std
     template<typename T, typename... Args>
     unique_ptr<T> make_unique(Args&&... args)
     {
-        return unique_ptr<T>{new T{ args... }};
+        return unique_ptr<T>{new T{ forward<Args>(args)... }};
     }
 }
 #endif
