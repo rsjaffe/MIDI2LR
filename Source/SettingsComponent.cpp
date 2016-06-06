100c71ce6bf16d609ab127eabc3fe2b3a607b01e
rsjaffe <rsjaffe@gmail.com>
2016-06-05 22:25:35 -0700

    White space in comments

--- Source/SettingsComponent.cpp
+++ Source/SettingsComponent.cpp
@@ -7,7 +7,8 @@ This file is part of MIDI2LR. Copyright 2015-2016 by Rory Jaffe.
 
 MIDI2LR is free software: you can redistribute it and/or modify it under the
 terms of the GNU General Public License as published by the Free Software
-Foundation, either version 3 of the License, or (at your option) any later version.
+Foundation, either version 3 of the License, or (at your option) any later 
+version.
 
 MIDI2LR is distributed in the hope that it will be useful, but WITHOUT ANY
 WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
