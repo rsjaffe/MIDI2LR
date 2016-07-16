02a386a98d22eb8bd991d6c1cd322f963a1de700
Rory Jaffe <rsjaffe@gmail.com>
2016-07-15 21:11:50 -0700

    another observer pattern

---
+++ Source/Utilities/EventType.h
@@ -0,0 +1,14 @@
+//
+// Copyright (c) 2013 Juan Palacios juan.palacios.puyana@gmail.com
+// Subject to the BSD 2-Clause License
+// - see < http://opensource.org/licenses/BSD-2-Clause>
+//
+
+#ifndef EVENTTYPE_H_
+#define EVENTTYPE_H_
+
+enum class EventType { CONNECTED, DISCONNECTED, BLUE, YELLOW, ORANGE };
+
+#endif
+
+
