dc5819ee9fba603b9a317ab803f9f1512bbbfd79
rsjaffe <rsjaffe@gmail.com>
2016-11-27 10:14:51 -0800

    Rearrange controls on "other" tab

--- Source/LRPlugin/MIDI2LR.lrplugin/Options.lua
+++ Source/LRPlugin/MIDI2LR.lrplugin/Options.lua
@@ -66,9 +66,11 @@ local function setOptions()
             identifier = 'othersettings',
             f:view(Limits.StartDialog(properties,f)),
             f:separator {fill_horizontal = 0.9},
-            f:checkbox {title = LOC("$$$/AgDocument/ModulePicker/Settings/ShowStatusAndActivity=Show status and activity"), value = LrView.bind('ClientShowBezelOnChange')},
-            f:checkbox {title = LOC("$$$/MIDI2LR/Options/RevealAdjustedControls=Reveal adjusted controls"), value = LrView.bind('RevealAdjustedControls')},
-            OU.slider(f,properties,LOC("$$$/MIDI2LR/Options/TrackingDelay=Tracking Delay"),'slidersets','TrackingDelay',0,3,2),
+            f:row {
+              OU.slider(f,properties,LOC("$$$/MIDI2LR/Options/TrackingDelay=Tracking Delay"),'slidersets','TrackingDelay',0,3,2),
+              f:checkbox {title = LOC("$$$/AgDocument/ModulePicker/Settings/ShowStatusAndActivity=Show status and activity"), value = LrView.bind('ClientShowBezelOnChange')},
+              f:checkbox {title = LOC("$$$/MIDI2LR/Options/RevealAdjustedControls=Reveal adjusted controls"), value = LrView.bind('RevealAdjustedControls')},
+            }, --row
             f:separator {fill_horizontal = 0.9},
             Keys.StartDialog(properties,f),
           }, -- tab_view_item
