1a2132b8ff59c7bb1d17f07815dfaf133f827fcf
rsjaffe <rsjaffe@gmail.com>
2016-12-24 11:55:51 -0800

    Update copyright to 2015-2017

--- Source/SettingsManager.cpp
+++ Source/SettingsManager.cpp
@@ -3,7 +3,7 @@
 
     SettingsManager.cpp
 
-This file is part of MIDI2LR. Copyright 2015-2016 by Rory Jaffe.
+This file is part of MIDI2LR. Copyright 2015-2017 by Rory Jaffe.
 
 MIDI2LR is free software: you can redistribute it and/or modify it under the
 terms of the GNU General Public License as published by the Free Software
