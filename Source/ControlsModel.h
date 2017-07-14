28279633817f78f0c7fc27caf237f73107b9e192
Rory Jaffe <rsjaffe@gmail.com>
2017-07-13 19:09:22 -0700

    Add missing include

--- Source/ControlsModel.h
+++ Source/ControlsModel.h
@@ -28,6 +28,7 @@ MIDI2LR.  If not, see <http://www.gnu.org/licenses/>.
 #include <cereal/access.hpp>
 #include <cereal/types/array.hpp>
 #include <cereal/types/vector.hpp>
+#include "MidiUtilities.h"
 #include "Misc.h"
 
 namespace RSJ {
