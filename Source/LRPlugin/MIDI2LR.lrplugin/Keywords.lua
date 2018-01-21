3c28883b90393e1f31047e2b3cfbd6c61ee6860c
Rory Jaffe <rsjaffe@gmail.com>
2018-01-20 21:02:07 -0800

    fix copyright messages

--- Source/LRPlugin/MIDI2LR.lrplugin/Keywords.lua
+++ Source/LRPlugin/MIDI2LR.lrplugin/Keywords.lua
@@ -16,7 +16,6 @@ PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License along with
 MIDI2LR.  If not, see <http://www.gnu.org/licenses/>. 
-
 ---------------------------------------------------------------------------------------]]
 
 
