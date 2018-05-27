1261b636db536168f09718a0b4c9d21fa275b88e
rsjaffe <rsjaffe@gmail.com>
2018-05-27 10:36:33 -0700

    fix bash syntax error

--- compile.sh
+++ compile.sh
@@ -3,7 +3,7 @@ hash luac 2>/dev/null
 if [ $? -eq 0 ]
 mkdir -p Source/LRPlugin/MIDI2LR.lrplugin/compiled
 pushd Source/LRPlugin/MIDI2LR.lrplugin
-for filename in ./*.lua do luac -o "compiled/${filename}" "${filename}"; done
+for filename in ./*.lua; do luac -o "compiled/${filename}" "${filename}"; done
 popd
 fi
 /usr/bin/xcodebuild -configuration Release -project Builds/MacOSX/MIDI2LR.xcodeproj
