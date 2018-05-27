5503f829b4df754de505a68f2a9579bc18717220
Rory Jaffe <rsjaffe@gmail.com>
2018-05-27 14:38:27 -0700

    Fix compile.sh syntax, mark as executable

--- compile.sh
+++ compile.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-hash luac 2>/dev/null
-if [ $? -eq 0 ]
-mkdir -p Source/LRPlugin/MIDI2LR.lrplugin/compiled
-pushd Source/LRPlugin/MIDI2LR.lrplugin
-for filename in ./*.lua; do luac -o "compiled/${filename}" "${filename}"; done
-popd
+if ! hash luac 2>/dev/null
+then
+	mkdir -p Source/LRPlugin/MIDI2LR.lrplugin/compiled
+	pushd Source/LRPlugin/MIDI2LR.lrplugin || exit
+	for filename in *.lua; do luac -o "compiled/${filename}" "${filename}"; done
+	popd
 fi
 /usr/bin/xcodebuild -configuration Release -project Builds/MacOSX/MIDI2LR.xcodeproj
