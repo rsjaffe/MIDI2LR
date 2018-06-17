0e1375487d1c24b2a8957d7c11bf613e2a2c571a
Rory Jaffe <rsjaffe@gmail.com>
2018-06-16 18:55:19 -0700

    update jucer (not juce library)

--- JuceLibraryCode/BinaryData.cpp
+++ JuceLibraryCode/BinaryData.cpp
@@ -294,8 +294,7 @@ static const unsigned char temp_binary_data_0[] =
 const char* MIDI2LR_png = (const char*) temp_binary_data_0;
 
 
-const char* getNamedResource (const char*, int&) throw();
-const char* getNamedResource (const char* resourceNameUTF8, int& numBytes) throw()
+const char* getNamedResource (const char* resourceNameUTF8, int& numBytes)
 {
     unsigned int hash = 0;
     if (resourceNameUTF8 != 0)
@@ -309,12 +308,28 @@ const char* getNamedResource (const char* resourceNameUTF8, int& numBytes) throw
     }
 
     numBytes = 0;
-    return 0;
+    return nullptr;
 }
 
 const char* namedResourceList[] =
 {
     "MIDI2LR_png"
 };
+
+const char* originalFilenames[] =
+{
+    "MIDI2LR.png"
+};
+
+const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8)
+{
+    for (unsigned int i = 0; i < (sizeof (namedResourceList) / sizeof (namedResourceList[0])); ++i)
+    {
+        if (namedResourceList[i] == resourceNameUTF8)
+            return originalFilenames[i];
+    }
+
+    return nullptr;
+}
 
 }
