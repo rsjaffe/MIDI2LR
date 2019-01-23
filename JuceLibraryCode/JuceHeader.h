a0cbb1d4f32e14eb7c109dcdf3eb2e21ce949f9e
Unknown <rsjaffe@gmail.com>
2019-01-23 14:17:15 -0800

    Retarget windows build to latest and update PVS studio comment
    Revised PVS studio license
    
    remove comments

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -34,6 +34,7 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "MIDI2LR";
+    const char* const  companyName    = "";
     const char* const  versionString  = "3.0.0.0";
     const int          versionNumber  = 0x3000000;
 }
--- Source/ResizableLayout.cpp
+++ Source/ResizableLayout.cpp
@@ -1,5 +1,3 @@
-// This is an open source non-commercial project. Dear PVS-Studio, please check it.
-// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 /*******************************************************************************
 "A Collection of Useful C++ Classes for Digital Signal Processing"
 By Vinnie Falco
