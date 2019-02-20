34a041b13bbc7c47403ee94416e378ca9905b3c2
Unknown <rsjaffe@gmail.com>
2019-02-20 10:49:57 -0800

    Replacements for CommandMenu structures

--- Source/CommandSet.cpp
+++ Source/CommandSet.cpp
@@ -4,17 +4,20 @@
 namespace fs = std::filesystem;
 #endif
 #include <fstream>
-#include <JuceLibraryCode/JuceHeader.h>
-#include "Misc.h"
 
 CommandSet::CommandSet() : m_impl(make_impl())
 {
    size_t idx = 0;
-   for (const auto& bycategory : m_impl.allcommands_)
+   for (const auto& bycategory : m_impl.allcommands_) {
+      menus_.push_back(bycategory.first);
+      std::vector<MenuStringT> menu_items_temp{};
       for (const auto& cmd_pair : bycategory.second) {
          cmd_by_number_.push_back(cmd_pair.first);
-         cmd_idx_[cmd_pair.first] = idx++;
+         cmd_idx_[cmd_pair.second] = idx++;
+         menu_items_temp.push_back(cmd_pair.second);
       }
+      menu_entries_.emplace_back(std::move(menu_items_temp));
+   }
 }
 
 CommandSet::~CommandSet() = default; // defined in cpp file in case go to pimpl idiom for Impl
@@ -22,7 +25,7 @@ CommandSet::~CommandSet() = default; // defined in cpp file in case go to pimpl 
 CommandSet::Impl::Impl()
 {
    auto& ls{*this}; // will use for running cereal
-   { // scoped so archive gets flushed
+   {                // scoped so archive gets flushed
       try {
 #ifdef _WIN32
          fs::path p{rsj::AppDataFilePath("MenuTrans.xml")};
@@ -40,7 +43,8 @@ CommandSet::Impl::Impl()
 #endif
          }
          else
-            rsj::LogAndAlertError("Unable to load control settings from xml file. Unable to open file");
+            rsj::LogAndAlertError(
+                "Unable to load control settings from xml file. Unable to open file");
       }
       catch (const std::exception& e) {
          rsj::ExceptionResponse(typeid(this).name(), __func__, e);
@@ -55,11 +59,11 @@ const CommandSet::Impl& CommandSet::make_impl()
    return singleimpl;
 }
 
-size_t CommandSet::GetIndexOfCommand(const std::string& command) const
+size_t CommandSet::CommandTextIndex(const MenuStringT& command) const
 {
    auto found = cmd_idx_.find(command);
    if (found == cmd_idx_.end()) {
-      rsj::LogAndAlertError("Command not found in GetIndexOfCommand: " + command);
+      rsj::LogAndAlertError("Command not found in CommandTextIndex: " + command);
       return 0;
    }
    return found->second;
--- Source/CommandSet.h
+++ Source/CommandSet.h
@@ -8,26 +8,40 @@
 #include <string>
 #include <unordered_map>
 #include <vector>
+#include <JuceLibraryCode/JuceHeader.h>
+#include "Misc.h"
 
 class CommandSet {
+   using MenuStringT = juce::String;
+
  public:
    CommandSet();
    ~CommandSet();
-   size_t GetIndexOfCommand(const std::string& command) const; // replaces
-                                                               // LrCommandList::getIndexOfCommand
-   const auto& CommandByIndex(size_t index) const
+   size_t CommandTextIndex(const MenuStringT& command) const; // replaces
+                                                              // LrCommandList::getIndexOfCommand
+   const auto& CommandAbbrevAt(size_t index) const
    { // replaces LrCommandList::LrStringList.at
       return cmd_by_number_.at(index);
    }
 
-   auto NumberOfCommands() const noexcept
+   auto CommandAbbrevSize() const noexcept
    { // replaces LrCommandList::LrStringList.size
       return cmd_by_number_.size();
    }
 
-   auto GetLanguage() const
+   auto GetLanguage() const noexcept
    {
       return m_impl.language_;
+   }
+
+   const auto& GetMenus() const noexcept
+   { // replaces menus_ in CommandMenu
+      return menus_;
+   }
+
+   const auto& GetMenuEntries() const noexcept
+   { // replaces menu_entries_ in CommandMenu
+      return menu_entries_;
    }
 
  private:
@@ -44,6 +58,9 @@ class CommandSet {
          if (version == 1)
             archive(cereal::make_nvp("language", language_),
                 cereal::make_nvp("all_commands", allcommands_));
+         else
+            rsj::LogAndAlertError(
+                "Unsupported archive version for CommandSet. Version is " + version);
       }
       std::string language_;
       std::vector<std::pair<std::string, std::vector<std::pair<std::string, std::string>>>>
@@ -58,8 +75,10 @@ class CommandSet {
    const Impl& m_impl;
    const Impl& make_impl();
 
-   std::unordered_map<std::string, size_t> cmd_idx_{};
+   std::unordered_map<MenuStringT, size_t> cmd_idx_{}; // for CommandTextIndex
    std::vector<std::string> cmd_by_number_{}; // use for LrCommandList::LrStringList.at, .size
+   std::vector<MenuStringT> menus_{};         // use for commandmenu
+   std::vector<std::vector<MenuStringT>> menu_entries_{}; // use for commandmenu
 };
 #pragma warning(push)
 #pragma warning(disable : 26440 26426 26444)
