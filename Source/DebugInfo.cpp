cd3cfce4793481d213ef69bbb5720d6e72aaa0e9
Unknown <rsjaffe@gmail.com>
2019-03-02 12:01:53 -0800

    Minimize windows.h inclusion
    Yikes! 10s of thousands of lines of stuff include lots of #defines
    Sort defines: unsorted in windows.h

--- Source/DebugInfo.cpp
+++ Source/DebugInfo.cpp
@@ -24,11 +24,48 @@ MIDI2LR.  If not, see <http://www.gnu.org/licenses/>.
 #include <array>
 #include <unordered_map>
 #ifndef WIN32_LEAN_AND_MEAN
+#define NOATOM -Atom Manager routines
+#define NOCLIPBOARD -Clipboard routines
+#define NOCOLOR -Screen colors
+#define NOCOMM -COMM driver routines
+#define NOCTLMGR -Control and Dialog routines
+#define NODEFERWINDOWPOS -DeferWindowPos routines
+#define NODRAWTEXT -DrawText() and DT_*
+#define NOGDI -All GDI defines and routines
+#define NOGDICAPMASKS -CC_*, LC_*, PC_*, CP_*, TC_*, RC_
+#define NOHELP -Help engine interface.
+#define NOICONS -IDI_*
+#define NOKANJI -Kanji support stuff.
+#define NOKERNEL -All KERNEL defines and routines
+#define NOKEYSTATES -MK_*
+#define NOMB -MB_*and MessageBox()
+#define NOMCX -Modem Configuration Extensions
+#define NOMEMMGR -GMEM_*, LMEM_*, GHND, LHND, associated routines
+#define NOMENUS -MF_*
+#define NOMETAFILE -typedef METAFILEPICT
+#define NOMINMAX -Macros min(a, b) and max(a, b)
+#define NOMSG -typedef MSG and associated routines
+#define NONLS -All NLS defines and routines
+#define NOOPENFILE -OpenFile(), OemToAnsi, AnsiToOem, and OF_*
+#define NOPROFILER -Profiler interface.
+#define NORASTEROPS -Binary and Tertiary raster ops
+#define NOSCROLL -SB_*and scrolling routines
+#define NOSERVICE -All Service Controller routines, SERVICE_ equates, etc.
+#define NOSHOWWINDOW -SW_*
+#define NOSOUND -Sound driver routines
+#define NOSYSCOMMANDS -SC_*
+#define NOSYSMETRICS -SM_*
+#define NOTEXTMETRIC -typedef TEXTMETRIC and associated routines
+#define NOVIRTUALKEYCODES -VK_*
+#define NOWH -SetWindowsHook and WH_*
+#define NOWINMESSAGES -WM_*, EM_*, LB_*, CB_*
+#define NOWINOFFSETS -GWL_*, GCL_*, associated routines
+#define NOWINSTYLES -WS_*, CS_*, ES_*, LBS_*, SBS_*, CBS_*
+#define OEMRESOURCE -OEM Resource values
 #define WIN32_LEAN_AND_MEAN
+//#define NOUSER -All USER defines and routines
 #endif
-#ifndef NOMINMAX
-#define NOMINMAX
-#endif
+#undef NOUSER
 #include "Windows.h"
 // from
 // https://docs.microsoft.com/en-us/windows-hardware/manufacture/desktop/windows-language-pack-default-values
