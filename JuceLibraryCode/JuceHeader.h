7d58c769c232c2cb9dca5bfb9901826d17ce8175
Unknown <rsjaffe@gmail.com>
2019-03-13 14:55:37 -0700

    update version to 3.0.0.11

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -35,7 +35,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "MIDI2LR";
     const char* const  companyName    = "";
-    const char* const  versionString  = "3.0.0.10";
-    const int          versionNumber  = 0x300000a;
+    const char* const  versionString  = "3.0.0.11";
+    const int          versionNumber  = 0x300000b;
 }
 #endif
