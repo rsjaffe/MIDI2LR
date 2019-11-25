cbb1e45cf098f0989e57e75ae7618a2ba7558f08
Rory Jaffe <rsjaffe@gmail.com>
2019-11-25 08:31:36 -0800

    update version to 3.4.2.0

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -44,7 +44,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "MIDI2LR";
     const char* const  companyName    = "";
-    const char* const  versionString  = "3.4.1.0";
-    const int          versionNumber  = 0x3040100;
+    const char* const  versionString  = "3.4.2.0";
+    const int          versionNumber  = 0x3040200;
 }
 #endif
