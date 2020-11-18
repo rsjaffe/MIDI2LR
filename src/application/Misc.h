14b0fd4e7a4933ce88433230af790d20cbd702f4
Rory Jaffe <rsjaffe@gmail.com>
2020-11-18 13:40:19 -0800

    :building_construction: flush aarch64 denormals to zero
    FPCR is 64 bits https://developer.arm.com/docs/ddi0595/e/aarch64-system-registers/fpcr

--- src/application/Misc.h
+++ src/application/Misc.h
@@ -60,7 +60,16 @@ constexpr auto MacOS {true};
 #ifndef __ARM_ARCH
 #define MIDI2LR_FAST_FLOATS _mm_setcsr(_mm_getcsr() | 0x8040)
 #else
-#define MIDI2LR_FAST_FLOATS /* no fast floats in ARM */
+#define MIDI2LR_FPU_GETCW(fpcr) __asm__ __volatile__("mrs %0, fpcr" : "=r"(fpcr))
+#define MIDI2LR_FPU_SETCW(fpcr) __asm__ __volatile__("msr fpcr, %0" : : "r"(fpcr))
+#define MIDI2LR_FAST_FLOATS                                                                        \
+   {                                                                                               \
+      uint64_t status {};                                                                          \
+      MIDI2LR_FPU_GETCW(status);                                                                   \
+      status |= (1 << 24) | (1 << 19); /* FZ flag, FZ16 flag; flush denormals to zero  */          \
+      MIDI2LR_FPU_SETCW(status);                                                                   \
+   }                                                                                               \
+   static_assert(true, "require semi-colon after macro with this assert")
 #endif
 
 namespace rsj {
