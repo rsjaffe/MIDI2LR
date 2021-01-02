4230f605389a1dde8068755758539e89f9323812
Rory Jaffe <rsjaffe@gmail.com>
2021-01-02 10:14:59 -0800

    update wil
    formatting

--- src/application/Main.cpp
+++ src/application/Main.cpp
@@ -237,8 +237,8 @@ class MIDI2LRApplication final : public juce::JUCEApplication {
                      const auto result {juce::NativeMessageBox::showYesNoBox(
                          juce::AlertWindow::WarningIcon, juce::translate("MIDI2LR profiles"),
                          juce::translate("Profile changed. Do you want to save your changes? If "
-                                         "you "
-                                         "continue without saving, your changes will be lost."))};
+                                         "you continue without saving, your changes will be "
+                                         "lost."))};
                      if (result)
                         main_window_->SaveProfile();
                   }
