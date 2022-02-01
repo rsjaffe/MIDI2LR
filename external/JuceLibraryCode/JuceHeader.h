355487c0f8b65a6802899b6f141ec48dc639301b
Rory Jaffe <rsjaffe@gmail.com>
2022-02-01 13:06:33 -0800

    5.3.0.2

--- external/JuceLibraryCode/JuceHeader.h
+++ external/JuceLibraryCode/JuceHeader.h
@@ -38,7 +38,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "MIDI2LR";
     const char* const  companyName    = "Rory Jaffe";
-    const char* const  versionString  = "5.3.0.1";
-    const int          versionNumber  = 0x5030001;
+    const char* const  versionString  = "5.3.0.2";
+    const int          versionNumber  = 0x5030002;
 }
 #endif
